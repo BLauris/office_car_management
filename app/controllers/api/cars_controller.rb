8a7c0b8db648abcd913e9daf88a23b03b906c193
BLauris <bligzna.lauris@gmail.com>
2017-10-15 16:43:18 +0300

    Updated API::CarsController

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -5,13 +5,16 @@ class Api::CarsController < ApplicationController
     render json: cars, status: 200
   end
   
+  def all_reservations
+    user_cars = UserCar.active_reservations
+    render json: user_cars, each_serializer: UserCarsSerializer
+  end
+  
   def reservation_details
-    
-    user_cars = UserCar.where('taken_at >= ?', DateTime.now)
+    user_cars = UserCar.active_reservations
                        .where(car_id: params[:car_id])
                        .includes(:user)
                        
-    #  UserCarsSerializer.new(user_cars.first).to_json
     render json: user_cars, each_serializer: UserCarsSerializer
   end
 end
