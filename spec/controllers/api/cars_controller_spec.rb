29d56503c353ed2b09e68ec2dd899968bfeab892
BLauris <bligzna.lauris@gmail.com>
2017-10-15 19:02:43 +0300

    Removed some unused files

--- spec/controllers/api/cars_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Api::CarsController, type: :controller do
-
-end
--- spec/models/user_car_spec.rb
+++ spec/models/user_car_spec.rb
@@ -27,15 +27,4 @@ RSpec.describe UserCar, type: :model do
     expect(user_car.save).to eq(true)
   end
   
-  it "can't reserve in past" do
-    user_car = UserCar.create(
-      taken_at: now - 1.day, 
-      taken_till: now,
-      user_id: user.id,
-      car_id: car.id
-    )
-    
-    expect(user_car.errors.messages[:taken_till]).to include("you can't car reserve in past")
-  end
-  
 end
